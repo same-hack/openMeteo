46c4cf20195db0444db5aecb96fb2070db7555b0
same-hack <samehack.info@gmail.com>
2026-02-19 13:54:58 +0900

    リーフレット表示

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,18 @@
-import { Component, signal } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-root',
-  imports: [],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  template: `<div id="map" style="height:100vh;width:100%"></div>`,
 })
-export class App {
-  protected readonly title = signal('leaflet-min');
+export class App implements AfterViewInit {
+  ngAfterViewInit(): void {
+    const map = L.map('map').setView([35.681236, 139.767125], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; OpenStreetMap contributors',
+    }).addTo(map);
+  }
 }
